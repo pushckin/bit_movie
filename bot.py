c0576990c2ad2e92d5bd4144e4c707a702317a4a
Владимир Амшей <pushckin@gmail.com>
2023-01-13 07:16:57 +0300

    добавлен канал wilsocom, изменен код функции вызываюжей парсинг убраны отдельным скриптом.

--- bot.py
+++ bot.py
@@ -1,33 +1,22 @@
 from aiogram import Dispatcher, Bot, executor, types
 from config import API_TOKEN, API_YOUTUBE
+from pars import get_video_details, get_wilsa_details, reiting, wilsacom
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
-# from pars import *
-import json
-import requests
+
+
 
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
 
 
-vReiteng_id = 'UClmI-b4_ro8UJLaNbiS9Isw'
-
-# Функция, которая возвращает список видео с канала
-def get_videos_from_channel(vReiteng_id, API_YOUTUBE):
-    url = f'https://www.googleapis.com/youtube/v3/search?key={API_YOUTUBE}&channelId={vReiteng_id}&part=snippet,id&order=date&maxResults=5'
-    response = requests.get(url)
-    reiting = response.json()['items']
-    return reiting
-
 # конпка помощь
 btn_help = KeyboardButton("В_рейтинге")
+btn_wl = KeyboardButton('Wilsacom')
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
-kb.add(btn_help)
+kb.add(btn_help).add(btn_wl)
 
 # кнопки inline появляются в сообщение после нажатия кнопки помощь
-reiting = get_videos_from_channel(vReiteng_id, API_YOUTUBE)
-markup = types.InlineKeyboardMarkup(row_width=2)
-
 video_buttens3 = []
 for video in reiting:
     video_id = video['id']['videoId']
@@ -35,7 +24,16 @@ for video in reiting:
     btn_3 = types.InlineKeyboardButton(text=video_title, callback_data=f'video_{video_id}')
     video_buttens3.append(btn_3)
 
-markup.add(*video_buttens3)
+video_buttens2 = []
+for video_2 in wilsacom:
+    video2_id = video_2['id']['videoId']
+    video2_title = video_2['snippet']['title']
+    btn_2 = types.InlineKeyboardButton(text=video2_title, callback_data=f'video2_{video2_id}')
+    video_buttens2.append(btn_2)
+
+markup = types.InlineKeyboardMarkup(row_width=1).add(*video_buttens3)
+markup2 = types.InlineKeyboardMarkup(row_width=1).add(*video_buttens2)
+
 
 # оброботчик команд команда start, в эту функцию встраевается кнопка помощь
 @dp.message_handler(commands=['start'])
@@ -48,12 +46,12 @@ async def start_command(message: types.Message):
 async def show_inline_kb(message: types.Message):
     await bot.send_message(message.chat.id, "Привет! Выберите трейлер.", reply_markup=markup)
 
+@dp.message_handler(lambda message: message.text == "Wilsacom")
+async def show_inline_kb(message: types.Message):
+    await bot.send_message(message.chat.id, "Привет! Выберите видео от Wilsocom.", reply_markup=markup2)
 
 
-def get_video_details(video_id, API_YOUTUBE):
-    url = f'https://www.googleapis.com/youtube/v3/videos?key={API_YOUTUBE}&id={video_id}&part=snippet,contentDetails,statistics'
-    response = requests.get(url)
-    return response.json()
+
 
 @dp.callback_query_handler(lambda c: c.data.startswith('video_'))
 async def video_callback(callback_query: types.CallbackQuery):
@@ -63,13 +61,20 @@ async def video_callback(callback_query: types.CallbackQuery):
     video_details = get_video_details(video_id, API_YOUTUBE)
     # get the title and description of the video
     video_title = video_details['items'][0]['snippet']['title']
-    # video_description = video_details['items'][0]['snippet']['description']
     message = f'Title: {video_title}\nLink: {video_url}'
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.message.chat.id, message)
 
-# with open("videos.json", "w") as file:
-#     json.dump(reiting, file, indent=2)
+@dp.callback_query_handler(lambda c: c.data.startswith('video2_'))
+async def video2_callback(callback_query: types.CallbackQuery):
+    video2_id = callback_query.data.split('_')[1]
+    video2_url = f'https://www.youtube.com/watch?v={video2_id}'
+    video2_details = get_wilsa_details(video2_id, API_YOUTUBE)
+    video2_title = video2_details['items'][0]['snippet']['title']
+    message2 = f'Title: {video2_title}\nLink: {video2_url}'
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.message.chat.id, message2)
+
 
 
 if __name__ == "__main__":
--- pars.py
+++ pars.py
@@ -1,8 +1,11 @@
-from config import API_YOUTUBE
 import json
 import requests
+from config import API_YOUTUBE
+
+
 
 vReiteng_id = 'UClmI-b4_ro8UJLaNbiS9Isw'
+wilsa_id = 'UCt7sv-NKh44rHAEb-qCCxvA'
 
 # Функция, которая возвращает список видео с канала
 def get_videos_from_channel(vReiteng_id, API_YOUTUBE):
@@ -10,34 +13,22 @@ def get_videos_from_channel(vReiteng_id, API_YOUTUBE):
     response = requests.get(url)
     reiting = response.json()['items']
     return reiting
-
-
-# Используя функцию, мы можем получить список видео и создать кнопки Inline
-# для каждого видео с названием видео и идентификатором callback_data,
-# соответствующим идентификатору видео
-
 reiting = get_videos_from_channel(vReiteng_id, API_YOUTUBE)
-# with open("videos.json", "w") as file:
-#     json.dump(videos, file, indent=2)
-# markup = InlineKeyboardMarkup()
-# for video in reiting:
-#     video_id = video['id']['videoId']
-#     video_title = video['snippet']['title']
-#     button = InlineKeyboardButton(text=video_title, callback_data=f'video_{video_id}')
-#     markup.add(button)
 
+def get_video_wilacom(wilsa_id, API_YOUTUBE):
+    url = f'https://www.googleapis.com/youtube/v3/search?key={API_YOUTUBE}&channelId={wilsa_id}&part=snippet,id&order=date&maxResults=5'
+    response = requests.get(url)
+    wilsacom = response.json()['items']
+    return wilsacom
+wilsacom = get_video_wilacom(wilsa_id, API_YOUTUBE)
 
+def get_video_details(video_id, API_YOUTUBE):
+    url = f'https://www.googleapis.com/youtube/v3/videos?key={API_YOUTUBE}&id={video_id}&part=snippet,contentDetails,statistics'
+    response = requests.get(url)
+    return response.json()
 
-# async def show_inline_kb(message: types.Message):
-#     reiting = get_videos_from_channel(vReiteng_id, API_YOUTUBE)
-#     markup = types.InlineKeyboardMarkup(row_width=2)
-#
-#     btn = types.InlineKeyboardButton("кнопка1", callback_data="bt")
-#     btn_2 = types.InlineKeyboardButton("кнопка2", callback_data="bt 2")
-#     for video in reiting:
-#         video_id = video['id']['videoId']
-#         video_title = video['snippet']['title']
-#         btn_3 = types.InlineKeyboardButton(text=video_title, callback_data=f'video_{video_id}')
-#     btn_4 = types.InlineKeyboardButton("кнопка4", callback_data="bt 4")
-#     markup.add(btn).add(btn_2).add(btn_3).add(btn_4)
-#     await bot.send_message(message.chat.id, "Привет! Нажми на лубую кнопку, чтобы начать.", reply_markup=markup)
+def get_wilsa_details(video2_id, API_YOUTUBE):
+    url = f'https://www.googleapis.com/youtube/v3/videos?key={API_YOUTUBE}&id={video2_id}&part=snippet,contentDetails,statistics'
+    responseWilsa = requests.get(url)
+    return responseWilsa.json()
+
